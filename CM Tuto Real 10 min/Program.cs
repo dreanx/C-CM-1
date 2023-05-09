b114978d155aa94f4fbae36edf0d52cd27523e0c
dreanx <pierre.basck@gmail.com>
2023-05-09 14:33:21 +0200

    Add project files.

---
+++ CM Tuto Real 10 min/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CM_Tuto_Real_10_min
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i = 50;
+            string s = "test";
+            Console.WriteLine("Hello " + i + " " + s); 
+            TestFunction();
+            bool b = SecondTestFunction(55);
+            Console.WriteLine("Hello " + b);
+            Console.ReadKey();
+        }
+
+        static void TestFunction()
+        {
+
+        }
+
+        static bool SecondTestFunction(int i)
+        {
+            return i < 100;
+        }
+    }
+}
