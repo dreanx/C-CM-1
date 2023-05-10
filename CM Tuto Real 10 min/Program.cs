81d5c453b643c38682f88b6e87c7bcf62b0e4bc0
dreanx <pierre.basck@gmail.com>
2023-05-10 13:35:17 +0200

    boop

--- CM Tuto Real 10 min/Program.cs
+++ CM Tuto Real 10 min/Program.cs
@@ -15,19 +15,32 @@ namespace CM_Tuto_Real_10_min
             string s = "test";
             Console.WriteLine("Hello " + i + " " + s); 
             TestFunction();
-            bool b = SecondTestFunction(55);
-            Console.WriteLine("Hello " + b);
+            bool bFalse = SecondTestFunction(105);
+            bool bTrue = SecondTestFunction(50);
+            Console.WriteLine("Hello " + bFalse + " " + bTrue);
+
+            // Type your username and press enter
+            Console.WriteLine("Enter username:");
+
+            // Create a string variable and get user input from the keyboard and store it in the variable
+            string userName = Console.ReadLine();
+
+            // Print the value of the variable (userName), which will display the input value
+            Console.WriteLine("Username is: " + userName);
+
             Console.ReadKey();
         }
+    
+
 
         static void TestFunction()
-        {
+            {
 
-        }
+            }
 
         static bool SecondTestFunction(int i)
-        {
+            {
             return i < 100;
-        }
+            }
     }
 }
